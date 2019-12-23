168ae48f3cc45719f8991241fc43d4471742f8fb
Pedro Melo <pedroluiz_94@hotmail.com>
2019-12-22 23:40:07 -0300

    stater app test swift

---
+++ Conversores/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Pedro Luiz on 22/12/19.
+//  Copyright © 2019 Pedro Luiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+}
+
